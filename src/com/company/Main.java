32dd54465aee7999693d17fdbfac3ae203817c17
DLauraDL <127467430+DLauraDL@users.noreply.github.com>
2023-04-30 22:57:47 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+/*
+@author DIANA LAURA DOMINGUEZ LOPEZ
+@email diann.domm15@gmail.com
+@PALINDROMO
+*/
+package com.pperezm;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String cadena ="somos";
+        ArrayDeque <Character> pila = new ArrayDeque<>();
+        boolean esPalindromo = true;
+        // write your code here
+        for(int i=0; i< cadena.length();i++){
+            pila.push(cadena.charAt(i));
+            //System.out.println(cadena.charAt(i));
+
+        }
+
+        for(int j=0; j< cadena.length();j++) {
+            cadena.charAt(j);
+            //System.out.println("");
+            //System.out.println(cadena.charAt(j));
+            //System.out.println(j);
+
+            if (cadena.charAt(j) == pila.pop()) {
+                continue;
+
+            } else {
+                esPalindromo = false;
+                break;
+            }
+
+        }
+
+        String mensaje = esPalindromo?"Si es palindromo":"No es palindromo";
+        System.out.println(cadena);
+        System.out.println(mensaje);
+
+    }
+}
+
